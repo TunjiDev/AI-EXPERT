101cb87cc6b4b0dbd15a60f7eb03d22225a30223
TunjiDev <adetunjiigbatayo@gmail.com>
2026-09-25 02:29:43 +0100

    chore: update lesson materials

--- Module-4/Lesson23-Building_a_sentiment_analysis_tool_with_hugging_face/config.py
+++ Module-4/Lesson23-Building_a_sentiment_analysis_tool_with_hugging_face/config.py
@@ -1,2 +1,1 @@
-HF_API_KEY="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"
-API_URL="https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
+HF_API_KEY="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"
